db7974ca8d078f14c3a0238d5fd64570060685e1
Prem <pskambale3000@gmail.com.com>
2024-05-30 00:30:29 +0530

    design wishlist model

--- vijay_arts-dev/src/Components/UserComponents/Wishlist/Wishlist.jsx
+++ vijay_arts-dev/src/Components/UserComponents/Wishlist/Wishlist.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import './Wishlist.css'
+import React, { useState } from 'react';
+import './Wishlist.css';
+import EmptyItems from '../../CommonComponents/Empty/EmptyItems';
 const Wishlist = () => {
+  const [wishlist, setWishlist] = useState([]);
   return (
     <div className="wishlist-main-container">
       <div className="wishlist-header-container">
         <p>Wishlist</p>
       </div>
-      <div className="layout-setting-container">
+      {
+        wishlist.length === 0 ?
+          <EmptyItems
+            emptyItemName={'Wishlist'}
+            emptyDescription={`You don't have any products in the wishlist yet.`}
+          />
+          :
+          <div className="layout-main-container">
 
-      </div>
+          </div>
+
+      }
+
     </div>
   )
 }
