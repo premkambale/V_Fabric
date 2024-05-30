b3dcf3110c017d12e93f1bc4bd4541c33d2396cd
Shreyas Bansode <Shreyas Bansode@ho.rnt.ai>
2024-05-30 17:55:00 +0530

    responsive sidebar

--- vijay_arts-dev/src/Components/CommonComponents/Header/Header.jsx
+++ vijay_arts-dev/src/Components/CommonComponents/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.css'
 import { IoCallOutline, IoSearchOutline } from "react-icons/io5";
 import { CiMail } from "react-icons/ci";
@@ -7,14 +7,22 @@ import { LuUser } from "react-icons/lu";
 import { CiHeart, CiShoppingCart } from "react-icons/ci";
 import { useNavigate, NavLink } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import { CgMenuLeft } from "react-icons/cg";
+import { RiCloseLine } from "react-icons/ri";
 
 const Header = () => {
     const navigate = useNavigate();
     const location = useLocation()
     const path = location.pathname;
 
+    const [expandSidebar, setExpandSidebar] = useState(false)
+
     const navigationHandler = (navigationPath) => {
         navigate(`/main/${navigationPath}`)
+    }
+
+    const expandMenuBar = () => {
+        setExpandSidebar(true)
     }
 
     return (
@@ -35,26 +43,41 @@ const Header = () => {
                 </div>
             </div>
             <div className="headerContaiener">
+                <div className='menuBar' onClick={expandMenuBar}>
+                    <CgMenuLeft size={25} />
+                </div>
                 <div className="logosection">
                     <img src={logo} alt="error" />
                 </div>
-                <div className="collection-list">
+                <div className={expandSidebar ? 'Expandcollection-list' : "collection-list"}>
+                    <div className="manu-top-bar">
+                        <div className="left-side-menu">
+                            <p>MENU</p>
+                        </div>
+                        <div className="right-side-menu">
+                            <p>CATEGORIES</p>
+                        </div>
+                        <div className='ClosemenuBar' onClick={() => setExpandSidebar(false)}>
+                            <RiCloseLine color='white' size={20} />
+                        </div>
+                    </div>
+
                     <ul>
-                        <NavLink to='/main/newArrivals' style={{ textDecoration: 'none', color: "black" }}>
-                            <li style={{ color: path.includes('/main/newArrivals') ? '#e91e63' : 'black' }} >New Arrivals</li>
+                        <NavLink to='/main/newArrivals' style={{ textDecoration: 'none', color: "black", width: '100%' }}>
+                            <li style={{ color: path.includes('/main/newArrivals') ? '#e91e63' : 'black' }} className='list-label' onClick={() => setExpandSidebar(false)}>New Arrivals</li>
                         </NavLink>
-                        <NavLink to='/main/hotselling' style={{ textDecoration: 'none', color: "black" }} >
-                            <li className='collectionListInner' style={{ color: path.includes('/main/hotselling') ? '#e91e63' : 'black' }}>
+                        <NavLink to='/main/hotselling' style={{ textDecoration: 'none', color: "black", width: '100%' }} >
+                            <li className='collectionListInner' style={{ color: path.includes('/main/hotselling') ? '#e91e63' : 'black' }} onClick={() => setExpandSidebar(false)}>
                                 <span>Trendy</span>
                                 Hot Selling
                             </li>
                         </NavLink>
-                        <NavLink to='/main/lehengacholi' style={{ textDecoration: 'none', color: "black" }}  >
-                            <li style={{ color: path.includes('/main/lehengacholi') ? '#e91e63' : 'black' }}>Lehenga Choli</li>
+                        <NavLink to='/main/lehengacholi' style={{ textDecoration: 'none', color: "black", width: '100%' }} onClick={() => setExpandSidebar(false)} >
+                            <li style={{ color: path.includes('/main/lehengacholi') ? '#e91e63' : 'black' }} >Lehenga Choli</li>
                         </NavLink>
                         <li>Bridal Lehenga</li>
-                        <NavLink to='/main/sale' style={{ textDecoration: 'none', color: "black" }} >
-                            <li className='collectionListInner' style={{ color: path.includes('/main/sale') ? '#e91e63' : 'black' }}>
+                        <NavLink to='/main/sale' style={{ textDecoration: 'none', color: "black", width: '100%' }} >
+                            <li className='collectionListInner' style={{ color: path.includes('/main/sale') ? '#e91e63' : 'black' }} onClick={() => setExpandSidebar(false)}>
                                 <span className='saleBackground'>Sale</span>
                                 Sale
                             </li>
