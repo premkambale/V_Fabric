e3922d83a455d5c3107cd08014bd0eb5ca67f11d
Prem <pskambale3000@gmail.com.com>
2024-05-31 01:22:02 +0530

    Added Wishlist

--- vijay_arts-dev/src/Components/UserComponents/Wishlist/Wishlist.jsx
+++ vijay_arts-dev/src/Components/UserComponents/Wishlist/Wishlist.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
 import './Wishlist.css';
 import EmptyItems from '../../CommonComponents/Empty/EmptyItems';
+import { newArrivalsData } from '../Dashboard/Collections/NewArrivals/NewArrivals';
+import Card from '../../CommonComponents/Card/Card';
 const Wishlist = () => {
-  const [wishlist, setWishlist] = useState([]);
+  const [wishlist, setWishlist] = useState([...newArrivalsData]);
+  const [layoutType, setLayoutType] = useState(1)
+
+  const handleSelectLayout = (layoutNo) => {
+    setLayoutType(layoutNo)
+  }
   return (
     <div className="wishlist-main-container">
       <div className="wishlist-header-container">
@@ -15,10 +22,36 @@ const Wishlist = () => {
             emptyDescription={`You don't have any products in the wishlist yet.`}
           />
           :
-          <div className="layout-main-container">
-
+          <div className="wishlist-cards-main-container">
+            <div className="layout-setting-container">
+              <div className="layout-setting-inner-container">
+                <div className={`layout-btn ${layoutType === 1 && 'grid1'}`} onClick={() => handleSelectLayout(1)}>
+                  <span></span><span></span>
+                </div>
+                <div className={`layout-btn ${layoutType === 2 && 'grid2'}`} onClick={() => handleSelectLayout(2)}>
+                  <span></span><span></span>
+                </div>
+                <div className={`layout-btn ${layoutType === 3 && 'grid3'}`} onClick={() => handleSelectLayout(3)}>
+                  <span></span><span></span><span></span>
+                </div>
+                <div className={`layout-btn ${layoutType === 4 && 'grid4'}`} onClick={() => handleSelectLayout(4)}>
+                  <span></span><span></span><span></span><span></span>
+                </div>
+                <div className={`layout-btn ${layoutType === 5 && 'grid5'}`} onClick={() => handleSelectLayout(5)}>
+                  <span></span><span></span><span></span><span></span><span></span>
+                </div>
+              </div>
+            </div>
+            <div className="wishlist-cards-container  centered">
+              {
+                wishlist.map((item) => {
+                  return (
+                    <Card item={{ ...item, layoutType: layoutType }} />
+                  )
+                })
+              }
+            </div>
           </div>
-
       }
 
     </div>
